7bad5ecd73975eb1aa5351d672cea11c6f8888bf
Baptiste Faure <baptiste.faure18@yahoo.com>
2021-11-06 16:01:53 +0100

    get domaines by id and uniteEnseignementID

--- backend/controllers/domaines.js
+++ backend/controllers/domaines.js
@@ -21,6 +21,82 @@ module.exports.getDomaines = async (req, res) => {
     res.send(JSON.stringify(domaines));
 };
 
+module.exports.getDomaine = async (req, res) => {
+    
+    // Set headers
+    res.setHeader("Access-Control-Allow-Origin","*");
+    res.setHeader('Content-Type', 'application/json');
+  
+    // Get id from parameter and check if valid
+    if (ObjectId.isValid(req.params.domaineID) == false) {
+        // Invalid Id
+        const errorDescription = 'Please provide a valid _id.';
+        console.log(errorDescription);
+        res.status(500);
+        res.send(errorDescription);
+        return;
+    }
+    const domaineID = req.params.domaineID;
+  
+    // Get Domaine from database with mathing _id
+    const domaine = await Domaine.findOne({ _id: domaineID }).lean();
+    
+    // Check if null
+    if (domaine == undefined || domaine == null) {
+        const errorDescription = 'No Domaine with _id: ' + domaineID;
+        console.log(errorDescription);
+        res.status(500);
+        res.send(errorDescription);
+        return;
+    } else {
+        // Remove unnecessary properties
+        delete domaine.__v;
+  
+        // Return response
+        res.status(200);
+        res.send(JSON.stringify(domaine));
+    }
+};
+
+module.exports.getDomainesByUniteEnseignementID = async (req, res) => {
+    
+    // Set headers
+    res.setHeader("Access-Control-Allow-Origin","*");
+    res.setHeader('Content-Type', 'application/json');
+  
+    // Get id from parameter and check if valid
+    if (ObjectId.isValid(req.params.uniteEnseignementID) == false) {
+        // Invalid Id
+        const errorDescription = 'Please provide a valid uniteEnseignementID.';
+        console.log(errorDescription);
+        res.status(500);
+        res.send(errorDescription);
+        return;
+    }
+    const uniteEnseignementID = req.params.uniteEnseignementID;
+  
+    // Get all domaines from database with matching uniteEnseignementID
+    const domaines = await Domaine.find({ uniteEnseignement: uniteEnseignementID }).lean();
+    
+    // Check if null
+    if (domaines == undefined || domaines == null || domaines.length == 0) {
+        const errorDescription = 'No Domaine with uniteEnseignementID: ' + uniteEnseignementID;
+        console.log(errorDescription);
+        res.status(500);
+        res.send(errorDescription);
+        return;
+    } else {
+        // Remove unnecessary properties
+        domaines.forEach(domaine => {
+          delete domaine.__v;
+        });
+  
+        // Return response
+        res.status(200);
+        res.send(JSON.stringify(domaines));
+    }
+};
+
 module.exports.postDomaine = async (req, res) => {
    
     // Set headers
--- backend/routes/domaine.route.js
+++ backend/routes/domaine.route.js
@@ -4,6 +4,8 @@ const router = express.Router();
 const domaineController = require('../controllers/domaines');
 
 router.get('/', domaineController.getDomaines);
+router.get('/:domaineID', domaineController.getDomaine);
+router.get('/uniteEnseignementID/:uniteEnseignementID', domaineController.getDomainesByUniteEnseignementID);
 router.post('/', domaineController.postDomaine);
 
 module.exports = router;
