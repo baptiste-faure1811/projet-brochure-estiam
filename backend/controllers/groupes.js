52c40940c2e4f0a95137035ae605ed7ea9ab2aef
Baptiste Faure <baptiste.faure18@yahoo.com>
2021-11-06 12:38:59 +0100

    cleaning code

--- backend/controllers/groupes.js
+++ backend/controllers/groupes.js
@@ -3,19 +3,33 @@ const mongoose = require('mongoose');
 const { ObjectId } = require('bson');
 
 module.exports.getGroupes = async (req, res) => {
-    const groupes = await Groupe.find();
+    
+  // Set headers
     res.setHeader("Access-Control-Allow-Origin","*");
+    res.setHeader('Content-Type', 'application/json');
+  
+    // Get all groupes from database
+    const groupes = await Groupe.find().lean();
+
+    // Remove unnecessary properties
+    groupes.forEach(groupe => {
+        delete groupe.uniteEnseignement;
+        delete groupe.__v;
+    });
+  
+    // Return response
     res.status(200);
     res.send(JSON.stringify(groupes));
 };
 
 module.exports.postGroupe = async (req, res) => {
    
+    // Set headers
     res.setHeader("Access-Control-Allow-Origin","*");
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-    res.status(200);
  
+    // Create new object to save using data from parameters
     const groupe = new Groupe({
       _id: ObjectId(),
       name: "Groupe Name",
@@ -24,11 +38,20 @@ module.exports.postGroupe = async (req, res) => {
       uniteEnseignement: []
     });
     
-    const createdGroupe = await groupe.save()
+    // Save new object to database
+    await groupe.save()
     .then(() => {
-      console.log("Success")
-      res.send(JSON.stringify(groupe));
+        // Creation was successfull
+        res.status(200);
+        console.log("Groupe successfully created with _id: " + groupe._id)
+        res.send(JSON.stringify(groupe));
     })
-    .catch((err) => console.log(`Could not save groupe with id ${groupe._id}`, err));
+    .catch(err => {
+        // An error occured
+        res.status(500);
+        const errorDescription = `Could not create Groupe with _id ${groupe._id}.`;
+        console.log(errorDescription, err);
+        res.send(errorDescription + " Error message: " + err.message);
+    });
 
 };
--- backend/controllers/programmes.js
+++ backend/controllers/programmes.js
@@ -3,19 +3,33 @@ const mongoose = require('mongoose');
 const { ObjectId } = require('bson');
 
 module.exports.getProgrammes = async (req, res) => {
-    const programmes = await Programme.find();
+
+    // Set headers
     res.setHeader("Access-Control-Allow-Origin","*");
+    res.setHeader('Content-Type', 'application/json');
+
+    // Get all programmes from database
+    const programmes = await Programme.find().lean()
+
+    // Remove unnecessary properties
+    programmes.forEach(programme => {
+        delete programme.groups;
+        delete programme.__v;
+    });
+
+    // Return response
     res.status(200);
     res.send(JSON.stringify(programmes));
 };
 
 module.exports.postProgramme = async (req, res) => {
    
+    // Set headers
     res.setHeader("Access-Control-Allow-Origin","*");
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-    res.status(200);
  
+    // Create new object to save using data from parameters
     const programme = new Programme({
       _id: ObjectId(),
       name: "Programme Name",
@@ -23,10 +37,20 @@ module.exports.postProgramme = async (req, res) => {
       totalDuration: 200,
       groups: []
     });
-    const createdProgramme = await programme.save()
+
+    // Save new object to database
+    await programme.save()
     .then(() => {
-      console.log("Success")
-      res.send(JSON.stringify(programme));
+        // Creation was successfull
+        res.status(200);
+        console.log("Progamme successfully created with _id: " + programme._id)
+        res.send(JSON.stringify(programme));
     })
-    .catch((err) => console.log(`Could not save programme with id ${programme._id}`, err));
+    .catch(err => {
+        // An error occured
+        res.status(500);
+        const errorDescription = `Could not create Programme with _id ${programme._id}.`;
+        console.log(errorDescription, err);
+        res.send(errorDescription + " Error message: " + err.message);
+    });
 };
