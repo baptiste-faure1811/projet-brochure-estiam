e29fd72d4126fb76426946436f8553b59c10eabd
Franck-TAKUETE <92853352+Franck-TAKUETE@users.noreply.github.com>
2021-12-03 01:14:50 +0100

    Franck
    Campus

---
+++ frontend/src/app/paiement-creation/paiement-creation.component.ts
@@ -0,0 +1,56 @@
+import { Component, OnInit } from '@angular/core';
+import { Paiement } from '../paiement';
+import { PaiementService } from '../paiement.service';
+import { Location } from '@angular/common';
+
+@Component({
+  selector: 'app-paiement-creation',
+  templateUrl: './paiement-creation.component.html',
+  styleUrls: ['./paiement-creation.component.css']
+})
+export class PaiementCreationComponent implements OnInit {
+
+  paiement = {
+    exemple: {
+      nom: "",
+      info: ""
+    },
+    prixAnnuel: {
+      titre: "",
+      prix: null,
+      details: ""
+    },
+    fraisMobilite: {
+      titre: "",
+      prix: null,
+      details: ""
+    },
+    fraisMobiliteInternational: {
+      titre: "",
+      prix: null,
+      details: ""
+    },
+    paiementEchelonne: {
+      titre: "",
+      prix: null,
+      details: ""
+    }
+  } as unknown as Paiement;
+
+  constructor(
+    private paiementService: PaiementService,
+    private location: Location
+  ) {}
+
+  ngOnInit(): void {
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
+
+  create(): void {
+    this.paiementService.addPaiement(this.paiement).subscribe();
+    this.location.back();
+  }
+}
---
+++ frontend/src/app/paiement.service.ts
@@ -0,0 +1,96 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+
+import { Paiement } from "./paiement";
+import { Observable, Subject, of} from "rxjs";
+import { catchError, tap } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PaiementService {
+
+  private Paiements: Paiement[] = [];
+  private paiements$ = new Subject<Paiement[]>();
+  readonly url = "http://localhost:3000/paiements";
+
+  constructor(private http: HttpClient) {}
+
+  getPaiements() : Observable<Paiement[]>{
+    return this.http.get<Paiement[]>(this.url)
+  }
+
+  getPaiement(id: string): Observable<Paiement> {
+    const url = `${this.url}/detail/${id}`;
+    return this.http.get<Paiement>(url);
+  }
+
+  addPaiement(paiement: Paiement) : Observable<Paiement> {
+    console.log(`paiement : ${paiement.exemple.nom}`)
+    const newPaiement = {
+      exemple: {
+        nom: paiement.exemple.nom,
+        info: paiement.exemple.info
+      },
+      prixAnnuel: {
+        titre: paiement.prixAnnuel.titre,
+        prix: paiement.prixAnnuel.prix,
+        details: paiement.prixAnnuel.details
+      },
+      fraisMobilite: {
+        titre: paiement.fraisMobilite.titre,
+        prix: paiement.fraisMobilite.prix,
+        details: paiement.fraisMobilite.details
+      },
+      fraisMobiliteInternational: {
+        titre: paiement.fraisMobiliteInternational.titre,
+        prix: paiement.fraisMobiliteInternational.prix,
+        details: paiement.fraisMobiliteInternational.details
+      },
+      paiementEchelonne: {
+        titre: paiement.paiementEchelonne.titre,
+        prix: paiement.paiementEchelonne.prix,
+        details: paiement.paiementEchelonne.details
+      }
+    } as unknown as Paiement;
+    // return this.http.post<Paiement>(this.url, newPaiement)
+
+    return this.http.post<Paiement>(this.url, newPaiement);
+  }
+
+  deletePaiement(paiement: Paiement): Observable<Paiement> {
+    const newURl = this.url + "/" + paiement._id;
+    return this.http.delete<Paiement>(newURl);
+  }
+
+  updatePaiement(newPaiement: Paiement): Observable<Paiement> {
+    const updatedPaiement = {
+      _id:newPaiement._id,
+      exemple: {
+        nom: newPaiement.exemple.nom,
+        info: newPaiement.exemple.info
+      },
+      prixAnnuel: {
+        titre: newPaiement.prixAnnuel.titre,
+        prix: newPaiement.prixAnnuel.prix,
+        details: newPaiement.prixAnnuel.details
+      },
+      fraisMobilite: {
+        titre: newPaiement.fraisMobilite.titre,
+        prix: newPaiement.fraisMobilite.prix,
+        details: newPaiement.fraisMobilite.details
+      },
+      fraisMobiliteInternational: {
+        titre: newPaiement.fraisMobiliteInternational.titre,
+        prix: newPaiement.fraisMobiliteInternational.prix,
+        details: newPaiement.fraisMobiliteInternational.details
+      },
+      paiementEchelonne: {
+        titre: newPaiement.paiementEchelonne.titre,
+        prix: newPaiement.paiementEchelonne.prix,
+        details: newPaiement.paiementEchelonne.details
+      }
+    } as unknown as Paiement;
+    return this.http.put<Paiement>(this.url, updatedPaiement);
+  }
+}
